00cd80a5788a6df7bbd614f10a5a3385dd73ebc4
Krylov Yaroslav <krylov.yaroslav@gmail.com>
2021-09-02 23:33:27 +0300

    Add chained functions optimization benchmark

--- benchmark/suites/trivial/ureact.cpp
+++ benchmark/suites/trivial/ureact.cpp
@@ -58,3 +58,77 @@ static void ureact_signal_reaction( benchmark::State& state )
     }
 }
 BENCHMARK( ureact_signal_reaction )->Name( FULL_BENCHMARK_NAME( ureact_signal_reaction ) );
+
+
+
+static void ureact_chained_functions_baseline( benchmark::State& state )
+{
+    ureact::context ctx;
+    auto a = ctx.make_value( 1 );
+    auto d = ureact::make_function( a,
+        []( int a ) { //
+            return ( ( a + a ) * ( a + a ) ) + ( ( a + a ) * ( a + a ) );
+        } );
+
+    assert( d.get() == 8 );
+
+    int i = 0;
+    for( auto _ : state )
+    {
+        a <<= i;
+        assert( d.get() == 8 * i * i );
+        benchmark::DoNotOptimize( d.get() );
+        ++i;
+    }
+}
+BENCHMARK( ureact_chained_functions_baseline )
+    ->Name( FULL_BENCHMARK_NAME( ureact_chained_functions_baseline ) );
+
+static void ureact_chained_functions_separate( benchmark::State& state )
+{
+    ureact::context ctx;
+    auto a = ctx.make_value( 1 );
+
+    auto b1 = ureact::make_function( with( a, a ), std::plus<>() );
+    auto b2 = ureact::make_function( with( a, a ), std::plus<>() );
+    auto b3 = ureact::make_function( with( a, a ), std::plus<>() );
+    auto b4 = ureact::make_function( with( a, a ), std::plus<>() );
+
+    auto c1 = ureact::make_function( with( b1, b2 ), std::multiplies<>() );
+    auto c2 = ureact::make_function( with( b3, b4 ), std::multiplies<>() );
+
+    auto d = ureact::make_function( with( c1, c2 ), std::plus<>() );
+
+    assert( d.get() == 8 );
+
+    int i = 0;
+    for( auto _ : state )
+    {
+        a <<= i;
+        assert( d.get() == 8 * i * i );
+        benchmark::DoNotOptimize( d.get() );
+        ++i;
+    }
+}
+BENCHMARK( ureact_chained_functions_separate )
+    ->Name( FULL_BENCHMARK_NAME( ureact_chained_functions_separate ) );
+
+static void ureact_chained_functions_optimized( benchmark::State& state )
+{
+    ureact::context ctx;
+    auto a = ctx.make_value( 1 );
+    auto d = ( ( a + a ) * ( a + a ) ) + ( ( a + a ) * ( a + a ) );
+
+    assert( d.get() == 8 );
+
+    int i = 0;
+    for( auto _ : state )
+    {
+        a <<= i;
+        assert( d.get() == 8 * i * i );
+        benchmark::DoNotOptimize( d.get() );
+        ++i;
+    }
+}
+BENCHMARK( ureact_chained_functions_optimized )
+    ->Name( FULL_BENCHMARK_NAME( ureact_chained_functions_optimized ) );
